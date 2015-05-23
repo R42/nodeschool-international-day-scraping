54cea0fb9a6dcb48bf49f8a3a0832d52a8092d68
Thiago Dantas <thiago.teixeira.dantas@gmail.com>
2015-05-23 16:07:00 +0100

    fetch nodeinternational location

--- full-source.js
+++ full-source.js
@@ -1,39 +1,18 @@
-
 var Browser = require('./browser');
-
-/*
- *  The nodeschool international day web's page isn't a static page.
- *  The  Event list ( left side ) will be loaded on document ready.
- *  How can we create a list/json of places that the event will occur ?
- *
- *   - curl http://nodeschool.io/international-day/ will return only the 'layout'
- *   - cheerio will save your ass when the web page that you are scraping is static ( one shot )
- *
- *   solution:
- *     Selenium Webdriver
- * */
 
 var browser = Browser('http://localhost:4444/wd/hub');
 
-/*
- *  GOAL: Fetch full page source
- *
- *  we have to wait until event-list be fetched from nodeschool server
- *  http://nodeschool.io/international-day/events.csv
- *
- *  obvioulsy, no one will scrape if upfront we know ( soubessemos ) that exist an available endpoint to do that.
- *  much more easier curl http://nodeschool.io/international-day/events.csv and parse the csv data.
- *
- *
- * */
-
 browser
-  .get('http://nodeschool.io/international-day/')
-  .waitForElement('css selector',  '.event-list', 5000)
-  .source()
-  .then(console.log)
-  .fin(function(){ browser.quit(); })
-  .done();
+ .get('http://nodeschool.io/international-day/')
+ .waitForElement('css selector',  '.event-list', 5000)
+ .elementsByCssSelector('.event-list li')
+ .then(function(elements) {
+   elements.forEach(function(e) { e.text(print); });
 
+   function print(err, text) {
+    console.log('Location: ', text);
+   }
 
-
+  })
+ .fin(function(){ browser.quit(); })
+ .done();
